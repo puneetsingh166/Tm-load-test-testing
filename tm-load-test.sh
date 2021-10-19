f0a5769c9dbac0a9b3da3b1a8ef5c763378b34ef
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-19 19:08:35 +0530

    Update tm-load-test.sh

--- tm-load-test.sh
+++ tm-load-test.sh
@@ -6,7 +6,7 @@ my-cosmos-tester --help
 sleep 30
 curl http://137.184.201.190:26657
 my-cosmos-tester -c 1 -T 10 -r 10 -s 250 --broadcast-tx-method async --endpoints ws://137.184.201.190:26657/websocket  --stats-output ./stats.csv
-ssh -i testnet1.pem root@137.184.201.190
+ssh -i ${{ secrets.PRIVATE_KEY}} root@137.184.201.190
 sleep10
 docker rm -f cosmos_container
 docker rmi -f puneet166/cosmos-node
