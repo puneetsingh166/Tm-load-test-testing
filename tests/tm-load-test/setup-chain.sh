8ee99f34c5cf5da7f545fa582e5a53af0f2de60e
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-22 19:15:31 +0530

    Update setup-chain.sh

--- tests/tm-load-test/setup-chain.sh
+++ tests/tm-load-test/setup-chain.sh
@@ -1,44 +1,15 @@
 !/bin/bash
 sudo bash buid-container.sh
-# set -eux
-# docker build -t cosmos-chain .
-# the directory of this script, useful for allowing this script
-# to be run with any PWD
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Remove existing container instance
-# set +e
-# docker rm -f cosmos_c
-# set -e
-
-
-#pushd $DIR/../
-
-# Run new test container instance
-# docker run --name cosmos_container --mount type=bind,source="$(pwd)"/,target=/onomy --cap-add=NET_ADMIN -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it onomy-base /bin/bash /onomy/tests/gravity/container-scripts/reload-code.sh $NODES
-# docker run --name cosmos_c -d -p 26656:26656 -p 26657:26657 -p 1317:1317 -p 9090:9090  -it cosmos-chain /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh"
-# sudo bash /run-load-test/master-slave-test.sh 1 20 1000 250
-
-
-#!/bin/bash
-# set -eux
-
-# the directory of this script, useful for allowing this script
-# to be run with any PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Remove existing container instance
-# set +e
-# docker rm -f onomy_test_instance
-# set -e
-
-# NODES=3
-
+#Remove existing container instance
+set +e
+docker rm -f cosmos_test_instance
+set -e
 pushd $DIR/../
 
 # Run new test container instance
-docker run -d --name cosmos_test_instance --mount type=bind,source="$(pwd)"/,target=/onomy --cap-add=NET_ADMIN -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it cosmosbase /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh"
+docker run -d --name cosmos_test_instance -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it cosmoschain /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh"
 sleep 30
+echo "------------------------------------------------Access port-----------------------------
 curl http://0.0.0.0:26657
-echo "-------------------------------------------------------------------------------"
-#sudo bash run-load-test/master-slave.sh 1 15 200 2500 logs
