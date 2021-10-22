4fc04045a28ad6f63299b6754db13753c03aecf5
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-22 19:12:15 +0530

    Update buid-container.sh

--- tests/tm-load-test/buid-container.sh
+++ tests/tm-load-test/buid-container.sh
@@ -1,17 +1,2 @@
-#!/bin/bash
-#set -eux
-
-# this directy of this script
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#DOCKERFOLDER=$DIR/dockerfile
-#REPOFOLDER=$DIR/../..
-
-# change our directory sot hat the git arcive command works as expected
-#pushd $REPOFOLDER
-
-#docker system prune -a -f
-# Build base container
-#git archive --format=tar.gz -o $DOCKERFOLDER/onomy.tar.gz --prefix=onomy/ HEAD
-#pushd $DOCKERFOLDER
 cd ../../
-docker build --pull -f tests/tm-load-test/dockerfile/Dockerfile  -t cosmosbase .
+docker build --pull -f tests/tm-load-test/dockerfile/Dockerfile  -t cosmoschain .
