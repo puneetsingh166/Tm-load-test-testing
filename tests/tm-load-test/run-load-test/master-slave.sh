bd9733455ff90612dadb280fdd5b71b738703369
puneetsingh166 <puneet@leewayhertz.com>
2021-10-25 10:55:22 +0000

    some changes

--- tests/tm-load-test/run-load-test/master-slave.sh
+++ tests/tm-load-test/run-load-test/master-slave.sh
@@ -9,7 +9,7 @@ curl http://0.0.0.0:26657
 echo "--------------------------------------- test cases starting-----------------------------------------------------------"
 my-cosmos-tester master --expect-slaves 1 --bind localhost:26670 -c $connections -T $time -r $rate -s $size --broadcast-tx-method async --endpoints ws://0.0.0.0:26657/websocket &
 my-cosmos-tester slave --master ws://localhost:26670 &
-destdir=./$fileName.txt
+destdir=/root/logs/$fileName.txt
 while true
 do
 sleep 1
--- tests/tm-load-test/setup-chain.sh
+++ tests/tm-load-test/setup-chain.sh
@@ -1,11 +1,26 @@
-!/bin/bash
-sudo bash buid-container.sh
+#!/bin/bash
+set -eux
+
+Connections=$1
+Time=$2
+Rate=$3
+Size=$4
+Filename=$5
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+result=$( docker images -q cosmoschain )
+
+sudo bash buid-container.sh	
+# Remove existing container instance
+set +e
+docker rm -f cosmos_test_instance
+set -e
 
 pushd $DIR/../
 
+
 # Run new test container instance
-docker run -d --name cosmos_test_instance -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it cosmoschain /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh | bash /root/home/run-load-test/master-slave.sh 1 25 200 2500 logs"
+docker run -d --name cosmos_test_instance -v "$(pwd)"/target:/root/logs -p 9090:9090 -p 26657:26657 -p 1317:1317 -p 8545:8545 -it cosmoschain /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh | bash /root/home/run-load-test/master-slave.sh $Connections $Time $Rate $Size $Filename"
 sleep 30
 echo "------------------------------------------------Access port-----------------------------"
 curl http://0.0.0.0:26657
