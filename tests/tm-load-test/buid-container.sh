3ef217f925ebf0e2a9cdc5f02b681aa36a1ac744
puneetsingh166 <90913214+puneetsingh166@users.noreply.github.com>
2021-10-25 17:03:15 +0530

    Update buid-container.sh

--- tests/tm-load-test/buid-container.sh
+++ tests/tm-load-test/buid-container.sh
@@ -1,2 +1,7 @@
-cd ../../
+#!/bin/bash
+set -eux
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKERFOLDER=$DIR/dockerfile
+REPOFOLDER=$DIR/../..
+pushd $REPOFOLDER
 docker build --pull -f tests/tm-load-test/dockerfile/Dockerfile  -t cosmoschain .
