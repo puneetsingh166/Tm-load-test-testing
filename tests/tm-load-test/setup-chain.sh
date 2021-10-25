322936a58b5717d4db485f2234f77f6751c4fe94
sunnyk56 <sunnyk@leewayhertz.com>
2021-10-25 19:13:19 +0530

    update some files

--- tests/tm-load-test/setup-chain.sh
+++ tests/tm-load-test/setup-chain.sh
@@ -21,5 +21,4 @@ pushd $DIR/../
 
 # Run new test container instance
 docker run -d --name cosmos_test_instance -v "$(pwd)"/target:/root/logs -p 9090:9090 -p 26657:26657 -p 1317:1317 -it cosmoschain /bin/bash -c "sleep infinity | bash /root/home/master-validator/init.sh | bash /root/home/run-load-test/master-slave.sh $CONNECTIONS $TIME $RATE $SIZE $FILE_NAME"
-echo "-----------------testing logs---------------------"
-cat "$(pwd)"/target/$FILE_NAME
+sleep 10
